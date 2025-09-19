10f79c6795b6ff4e694a011d0055c5d5c05411a8
Michael Hanna <mh2parker@gmail.com>
2025-09-19 20:41:48 +0000

    many deletions

--- a_an/upload_graph.py
+++
@@ -1,8 +0,0 @@
-#%%
-from circuit_tracer.graph import Graph
-from circuit_tracer.frontend.upload_graph_to_s3 import upload_graph_to_s3
-#%%
-graph = Graph.from_pt('attribution_graphs/qwen3-14b-relu-lowl0/a-banker.pt')
-# %%
-upload_graph_to_s3(graph, 'qwen3-14b-relu-lowl0-a-banker')
-# %%
