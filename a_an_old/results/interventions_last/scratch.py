227a46ff460ecdd9b9e3b5943b0b24414fc96b48
Michael Hanna <mh2parker@gmail.com>
2025-09-24 15:31:08 +0000

    anonymizing

--- a_an_old/results/interventions_last/scratch.py
+++
@@ -1,9 +0,0 @@
-#%%
-import pandas as pd
-#%%
-df = pd.read_csv('Qwen3-14B.csv')
-# %%
-df[df['correct_articles'] == 'an']['selected_nodes_count'].mean()
-# %%
-df[df['correct_articles'] == 'a']['selected_nodes_count'].mean()
-# %%
